ad848d54efca9aefb9d7610f2b1ba9374173a2c1
Yasuhiro Inami <inamiy@gmail.com>
2013-03-09 22:13:11 +0900

    Add demo.

---
+++ Demo/JJPrivateKeyEventsDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  JJPrivateKeyEventsDemo
+//
+//  Created by Yasuhiro Inami on 2013/03/09.
+//  Copyright (c) 2013年 Yasuhiro Inami. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
